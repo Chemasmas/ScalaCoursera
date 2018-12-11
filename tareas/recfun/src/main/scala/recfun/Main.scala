8f58f2d7a212d0e0b9102c89d33a6ca8f991590e
Jorge Sierra <jorge.sierra.florido@gmail.com>
2018-12-10 20:16:29 -0600

    Tarea 2 por hacer

--- tareas/recfun/src/main/scala/recfun/Main.scala
+++ tareas/recfun/src/main/scala/recfun/Main.scala
@@ -13,15 +13,37 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
-  
+    def pascal(c: Int, r: Int): Int = if (c==0 || c==r) 1 else pascal(c-1,r-1)+pascal(c,r-1)
+
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+
+      def clean(charsL: List[Char] ): List[Char] = charsL.filter( p => p == '(' || p == ')')
+
+      def evalRec(i: Int, chars: List[Char]):Boolean =  if (i<0) false else if(chars.isEmpty) if( i == 0) true else false
+                                                        else if (chars.head == '(') evalRec(i+1,chars.tail) else evalRec(i-1,chars.tail)
+
+      evalRec(0,clean(chars) )
+    }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = {
+      def hasOneAtLeast(money:Int ,coins:List[Int]):Boolean = coins.map( x => money%x == 0 ).reduce( (a,b) => a | b )
+
+      def clean(money:Int,coins:List[Int]):List[Int] = coins.filter(x=>x<=money)
+
+      def generateWays(money:Int,coin:Int):List[Int] = {println(money);println(coin);println(money/coin); 0 to (money/coin) toList}
+
+      //def calculate(money:Int,coins:List[Int]):Int => generateWays(money,coins.head)
+
+      println( generateWays(money,clean(money,coins).sorted.reverse.head)  )
+      //println(generateWays(300,200))
+      //print(clean(money,coins).sorted.reverse)
+      if (!hasOneAtLeast(money,clean(money,coins))) 0
+      else -1
+    }
   }
